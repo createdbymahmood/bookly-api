75d28bea195dfade7ad058654a9c7e358b434fb8
Mahmood Bagheri <createdbymahmood@gmail.com>
2021-01-07 20:38:12 +0330

    feat:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { CommentModule } from './comment/comment.module';
+import { PublisherModule } from './publisher/publisher.module';
+import { BookModule } from './book/book.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
+  imports: [UserModule, CommentModule, PublisherModule, BookModule, CategoryModule],
 })
 export class AppModule {}
