e9873c3fe0a5cf063c558f2a610a901dc4450fc9
Mahmood Bagheri <createdbymahmood@gmail.com>
2021-01-26 14:34:06 +0330

    feat:

--- src/publisher/dto/update-publisher.dto.ts
+++ src/publisher/dto/update-publisher.dto.ts
@@ -1,11 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsOptional } from 'class-validator';
 import { IsBookIdValid } from 'validations/IsBookIdValid';
 import { IsImageIdValid } from 'validations/isImageIdValid';
 import { IsUserIdValid } from 'validations/isUserIdValid';
 import { CreatePublisherDto } from './create-publisher.dto';
 
 export class UpdatePublisherDto extends PartialType(CreatePublisherDto) {
+    @IsOptional()
     title: string;
+
+    @IsOptional()
     description: string;
 }
 
